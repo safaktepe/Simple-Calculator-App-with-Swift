072bc9c16f608f3bc051d7107ec981a57f1f4a09
Mert Safaktepe <safaktepemert@gmail.com>
2022-05-31 17:52:24 +0300

    Basic math operations implemented.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonStackView: UIStackView!
     @IBOutlet weak var buttonsContainer: UIView!
     
+    //MARK: Variables
+    
+    var currentOperations: String  = ""
+    
+    
     //MARK: View
     override func viewDidLoad() {
         super.viewDidLoad()
-        //clearAllLabels()
+        clearAllLabels()
         buttonsContainer.layer.cornerRadius = 35
         buttonStackView.layoutMargins = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
         buttonStackView.isLayoutMarginsRelativeArrangement = true
@@ -32,28 +37,63 @@ class ViewController: UIViewController {
         .lightContent
     }
     
+  
     //MARK: Actions
     
     
     @IBAction func deleteLastButtonClicked(_ sender: Any) {
-        
+        if(!currentOperations.isEmpty){
+            currentOperations.removeLast()
+            operationLabel.text = currentOperations
+        }
     }
     @IBAction func deleteAllButtonClicked(_ sender: Any) {
         clearAllLabels()
     }
+    
     @IBAction func numbersButtonClicked(_ sender: UIButton) {
-        print(sender.currentTitle ?? 0)
+      //  print(sender.currentTitle ?? 0)
+        addToCurrentOperations(value: sender.currentTitle ?? "2")
     }
     
-    @IBAction func operationButtonClicked(_ sender: UIButton) {
-        print(sender.currentTitle ?? "+")
+    func addToCurrentOperations(value: String) {
+        currentOperations = currentOperations + value
+        operationLabel.text = currentOperations
     }
     
-    //MARK: Functions used for button's actions.
+    @IBAction func operationButtonTapped(_ sender: UIButton) {
+        //print(sender.currentTitle ?? "+")
+        addToCurrentOperations(value: sender.currentTitle!)
+
+    }
+    @IBAction func equalsTapped(_ sender: Any) {
+        let expression = NSExpression(format: currentOperations)
+        let result = expression.expressionValue(with: nil, context: nil) as! Double
+        let resultAsString = formatResult(result: result)
+        operationLabel.text = resultAsString
+        
+    }
+    
+    
+    
+    func formatResult(result: Double) -> String
+        {
+            if(result.truncatingRemainder(dividingBy: 1) == 0)
+            {
+                return String(format: "%.0f", result)
+            }
+            else
+            {
+                return String(format: "%.2f", result)
+            }
+        }
+    
+    //MARK: Functions used for buttons.
     func clearAllLabels() {
         operationLabel.text = nil
         resultLabel.text = nil
-        }
+        currentOperations = ""
+    }
     
 }
 
